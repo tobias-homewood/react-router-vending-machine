820430fa274f83f1f5120e1fa246af39ffb99a0d
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-11-25 18:07:30 +0000

    basic snack components, they just render a title and a button

---
+++ src/Chips.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Chips = () => {
+    return (
+        <div>
+            <h1>Chips</h1>
+            <button>Go back</button>
+        </div>
+    );
+};
+
+export default Chips;
---
+++ src/Chocolate.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Chocolate = () => {
+    return (
+        <div>
+            <h1>Chocolate</h1>
+            <button>Go back</button>
+        </div>
+    );
+};
+
+export default Chocolate;
